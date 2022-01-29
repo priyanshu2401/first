5cf3df35f86ac06108c604054068d9ecaf3665fd
Priyanshu Singh <97154677+priyanshu2401@users.noreply.github.com>
2022-01-29 15:28:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
-    window.onscroll = function (e) {
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight-300) {
+
+window.onscroll = function (e) {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight-160) {
         let bar = document.querySelectorAll(".common");
         bar.forEach((bar) => {
           let value = bar.getAttribute("data-done");
@@ -10,5 +11,22 @@
 
         // let load=document.querySelector('.bar');
       }
-    };
+};
+
+let navlist = document.querySelector(".nav-list");
+let navbar = document.querySelector(".navbar");
+let burger = document.querySelector(".burger");
+burger.addEventListener("click" , ()=>{
+    navlist.classList.toggle("v-resp");
+    navbar.classList.toggle("h-resp");
+    console.log("clicked");
+});
+    
+const focuselements = document.querySelectorAll(".focus");
+focuselements.forEach((focuselement)=>{
+    focuselement.addEventListener("click",()=>{
+      navlist.classList.toggle("v-resp");
+      navbar.classList.toggle("h-resp");
+    });
+});
   
